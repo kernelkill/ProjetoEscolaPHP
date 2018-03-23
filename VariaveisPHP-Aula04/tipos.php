e7dfbf8b17b7fca9830384ed81012299de653e8d
Kachorroski <kernelkill@outlook.com>
2018-03-23 15:33:22 -0400

    Projeto PHP da CEEP.

---
+++ VariaveisPHP-Aula04/tipos.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8"/>
+  <link rel="stylesheet" href="../_css/estilo.css"/>
+  <title></title>
+</head>
+<body>
+<div>
+  <?php
+
+
+  $nome = "Joabe";
+  $idade = 27;
+  echo $nome." tem ".$idade." anos!</br>";
+  echo "$nome tem $idade anos!";
+
+
+  ?>
+</div>
+</body>
+</html>
