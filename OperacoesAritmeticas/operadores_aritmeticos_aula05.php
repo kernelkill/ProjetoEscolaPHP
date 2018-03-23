878d1004eb9ffdf22ad3abb750515d40fbce7dca
Kachorroski <kernelkill@outlook.com>
2018-03-23 17:33:26 -0400

    Mergando

---
+++ OperacoesAritmeticas/operadores_aritmeticos_aula05.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../_css/estilo.css">
+    <title>Aula 05 - Operadores Aritmeticos</title>
+</head>
+<body>
+
+<div>
+    <?php
+/**
+ * Created by PhpStorm.
+ * User: kachorroski
+ * Date: 23/03/18
+ * Time: 16:32
+ */
+
+    $n1 = 2;
+    $n2 = 4;
+    $resu = $n1 + $n2;
+    echo "A soma de $n1 + $n2 é: $resu"
+
+    ?>
+</div>
+</body>
+</html>
+
--- OperaçoesAritmeticas/operadores_aritmeticos_aula05.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: kachorroski
- * Date: 23/03/18
- * Time: 16:32
- */
